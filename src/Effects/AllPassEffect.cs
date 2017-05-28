20fa8ad2df9ac6415c0b3cd50a5f76f5afb972db
igece <igece@igecelabs.net>
2017-05-29 00:17:47 +0200

    Implemented 'allpass' effect

---
+++ src/Effects/AllPassEffect.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+using SoxSharp.Effects.Types;
+
+
+namespace SoxSharp.Effects
+{
+  public class AllPassEffect : BaseEffect
+  {
+    public override string Name { get { return "allpass"; } }
+
+    public Frequency Frequency { get; set; }
+
+    public Width Width { get; set; }
+
+
+    public AllPassEffect(double frequency, double width)
+    {
+      Frequency = frequency;
+      Width = width;
+    }
+
+
+    public AllPassEffect(Frequency frequency, Width width)
+    {
+      Frequency = frequency;
+      Width = width;
+    }
+
+
+    public override string ToString()
+    {
+      StringBuilder effectArgs = new StringBuilder(Name);
+
+      effectArgs.Append(" " + Frequency);
+      effectArgs.Append(" " + Width);
+
+      return effectArgs.ToString();
+    }
+  }
+}
