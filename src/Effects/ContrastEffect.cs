4c2335b98cd9a4b6f54bd70b775fb7af0f11b4ca
igece <igece@igecelabs.net>
2017-05-30 23:15:08 +0200

    Implemented 'contrast' effect

---
+++ src/Effects/ContrastEffect.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+
+
+namespace SoxSharp.Effects
+{
+  /// <summary>
+  /// Comparable with compression, this effect modifies an audio signal to make it sound louder. 
+  /// </summary>
+  public class ContrastEffect : BaseEffect
+  {
+    public override string Name { get { return "contrast"; } }
+
+    /// <summary>
+    /// Controls the amount of the enhancement and is a number in the range 0−100 (default 75). Note that a value of 0
+    /// still gives a significant contrast enhancement.
+    /// </summary>
+    public double Enhancement { get; set; }
+
+
+    public ContrastEffect(double enhancement)
+    {
+      Enhancement = enhancement;
+    }
+
+
+    public override string ToString()
+    {
+      StringBuilder effectArgs = new StringBuilder(Name);
+      effectArgs.Append(" " + Enhancement);
+
+      return effectArgs.ToString();
+    }
+  }
+}
