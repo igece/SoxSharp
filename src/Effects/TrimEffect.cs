ed1a4a1beac61abc9e58d84e96b69376b2f0eaa3
igece <igece@igecelabs.net>
2017-06-02 06:55:50 +0200

    Revert "Documentation"
    This reverts commit c73d48ca3b6ea1e5975e5dc363f102abaaab9f75.

--- src/Effects/TrimEffect.cs
+++ src/Effects/TrimEffect.cs
@@ -32,11 +32,6 @@ namespace SoxSharp.Effects
     }
 
 
-    /// <summary>
-    /// Translate a <see cref="TrimEffect"/> instance to a set of command arguments to be passed to SoX.
-    /// (invalidates <see cref="object.ToString()"/>).
-    /// </summary>
-    /// <returns>A <see cref="T:System.String"/> containing SoX command arguments to apply a Trim effect.</returns>
     public override string ToString()
     {
       StringBuilder effectArgs = new StringBuilder(Name);
--- src/Test/Program.cs
+++ src/Test/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using SoxSharp;
-using SoxSharp.Exceptions;
 
 
 namespace Test
@@ -9,48 +8,35 @@ namespace Test
   {
     public static void Main(string[] args)
     {
-      try
+      using (Sox sox = new Sox(args[0]))
       {
-        using (Sox sox = new Sox(args[0]))
+        sox.OnLogMessage += (sender, e) =>
         {
-          sox.OnLogMessage += (sender, e) =>
-          {
-            Console.WriteLine(e.LogLevel + ": " + e.Message);
-          };
+          Console.WriteLine(e.LogLevel + ": " + e.Message);
+        };
 
-          sox.OnProgress += (sender, e) =>
-          {
-            Console.Write("Processing... {0}%   {1} {2} {3}                \r", e.Progress, e.Processed, e.Remaining, e.OutputSize);
-          };
+        sox.OnProgress += (sender, e) =>
+        {
+          Console.Write("Processing... {0}%   {1} {2} {3}                \r", e.Progress, e.Processed, e.Remaining, e.OutputSize);
+        };
 
-          Console.WriteLine("SoXSharp Test Application\n");
+        Console.WriteLine("SoXSharp Test App\n");
 
-          Console.WriteLine("File Information");
+        Console.WriteLine("File Information");
 
-          AudioInfo wavInfo = sox.GetInfo("test.wav");
-          Console.WriteLine(wavInfo);
+        FileInfo wavInfo = sox.GetInfo("test.wav");
+        Console.WriteLine(wavInfo);
 
-          Console.WriteLine("Simple Conversion");
+        Console.WriteLine("Simple Conversion");
 
-          sox.Output.Type = FileType.WAV;
-          sox.Output.SampleRate = 32000;
-          sox.Output.Comment = "Converted using SoX & SoXSharp";
+        sox.Output.Type = FileType.WAV;
+        sox.Output.SampleRate = 32000;
+        sox.Output.Comment = "Converted using SoX & SoXSharp";
 
-          sox.Process("test.wav", "test.xxx");
+        sox.Process("test.wav", "test.xxx");
 
-          Console.WriteLine("\r\nConversion finished");
-          Console.ReadKey();
-        }
-      }
-
-      catch (SoxException ex)
-      {
-        Console.WriteLine("SOXSHARP EXCEPTION RAISED: " + ex.Message + "\n\n" + ex.StackTrace);
-      }
-
-      catch (Exception ex)
-      {
-        Console.WriteLine("EXCEPTION RAISED: " + ex.Message + "\n\n" + ex.StackTrace);
+        Console.WriteLine("\r\nConversion finished");
+        Console.ReadKey();
       }
     }
   }
