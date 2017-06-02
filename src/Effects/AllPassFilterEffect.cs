433581ef38ac63fa109acb7cbb24c8df68dbf327
igece <igece@igecelabs.net>
2017-06-02 14:27:08 +0200

    Documentation

--- src/Effects/AllPassFilterEffect.cs
+++ src/Effects/AllPassFilterEffect.cs
@@ -31,12 +31,6 @@ namespace SoxSharp.Effects
     }
 
 
-    /// <summary>
-    /// Translate an <see cref="AllPassFilterEffect"/> instance to a set of command arguments to be passed to SoX.
-    /// (invalidates <see cref="object.ToString()"/>).
-    /// </summary>
-    /// <returns>A <see cref="T:System.String"/> containing SoX command arguments to apply an All-Pass Filter
-    /// effect.</returns>
     public override string ToString()
     {
       StringBuilder effectArgs = new StringBuilder(Name);
--- src/Effects/LowPassFilterEffect.cs
+++ src/Effects/LowPassFilterEffect.cs
@@ -54,11 +54,6 @@ namespace SoxSharp.Effects
     }
 
 
-    /// <summary>
-    /// Translate a <see cref="LowPassFilterEffect"/> instance to a set of command arguments to be passed to SoX.
-    /// (invalidates <see cref="object.ToString()"/>).
-    /// </summary>
-    /// <returns>A <see cref="T:System.String"/> containing SoX command arguments to apply a Low-Pass Filter effect.</returns>
     public override string ToString()
     {
       StringBuilder effectArgs = new StringBuilder(Name);
