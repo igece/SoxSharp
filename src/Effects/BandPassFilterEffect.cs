9c72036c0ed6e0a9c8700751707a3381fe3acc24
igece <igece@igecelabs.net>
2017-06-03 09:04:35 +0200

    Documentation

--- src/Effects/BandPassFilterEffect.cs
+++ src/Effects/BandPassFilterEffect.cs
@@ -36,6 +36,11 @@ namespace SoxSharp.Effects
     }
 
 
+    /// <summary>
+    /// Translate an <see cref="BandPassFilterEffect"/> instance to a set of command arguments to be passed to SoX.
+    /// (invalidates <see cref="object.ToString()"/>).
+    /// </summary>
+    /// <returns>A <see cref="T:System.String"/> containing SoX command arguments to apply an Band-Pass Filter effect.</returns>
     public override string ToString()
     {
       StringBuilder effectArgs = new StringBuilder(Name);
--- src/Effects/TrimEffect.cs
+++ src/Effects/TrimEffect.cs
@@ -6,6 +6,11 @@ using SoxSharp.Effects.Types;
 
 namespace SoxSharp.Effects
 {
+  /// <summary>
+  /// Cuts portions out of the audio. Any number of positions may be given; audio is not sent to the output until the
+  /// first position is reached. The effect then alternates between copying and discarding audio at each position. 
+  /// Using a value of 0 for the first position parameter allows copying from the beginning of the audio.
+  /// </summary>
   public class TrimEffect : BaseEffect
   {
     public override string Name { get { return "trim"; } }
@@ -32,6 +37,11 @@ namespace SoxSharp.Effects
     }
 
 
+    /// <summary>
+    /// Translate a <see cref="TrimEffect"/> instance to a set of command arguments to be passed to SoX.
+    /// (invalidates <see cref="object.ToString()"/>).
+    /// </summary>
+    /// <returns>A <see cref="T:System.String"/> containing SoX command arguments to apply a Trim effect.</returns>
     public override string ToString()
     {
       StringBuilder effectArgs = new StringBuilder(Name);
