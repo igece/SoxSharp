0475ac1eac48bd1292175b609213ec693de9d665
igece <igece@igecelabs.net>
2018-06-01 01:03:15 +0200

    Fixed TimeSpan generation to match SoX time specification string format.

--- src/Utils.cs
+++ src/Utils.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 using System.Linq;
 
 
@@ -6,12 +7,29 @@ namespace SoxSharp
 {
   static class Utils
   {
-    private static int[] weights = { 60 * 60 * 1000, 60 * 1000, 1000, 10 };
+    private static int[] weights2 = { 60, 1 };
+    private static int[] weights3 = { 60 * 60, 60, 1 };
 
 
     public static TimeSpan TimeSpanFromString(string time)
     {
-      return TimeSpan.FromMilliseconds(time.Split('.', ':').Zip(weights, (d, w) => Convert.ToInt64(d) * w).Sum());
+      string[] parts = time.Split(':');
+
+      switch (parts.Length)
+      {
+        case 1:
+          return TimeSpan.FromSeconds(Convert.ToDouble(parts[0], CultureInfo.InvariantCulture));
+
+        case 2:
+          return TimeSpan.FromSeconds(parts.Zip(weights2, (d, w) => String.IsNullOrEmpty(d) ? 0 : Convert.ToDouble(d , CultureInfo.InvariantCulture) * w).Sum());
+
+        case 3:
+          return TimeSpan.FromSeconds(parts.Zip(weights3, (d, w) => String.IsNullOrEmpty(d) ? 0 : Convert.ToDouble(d, CultureInfo.InvariantCulture) * w).Sum());
+
+        default:
+
+          throw new ArgumentException("Invalid time string");
+      }
     }
   }
 }
